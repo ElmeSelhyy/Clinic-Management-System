c0b1ff9a9a86afc0165eb09273c673c73f0f17e9
ahmedyasser20 <ahmedyasser200211@gmail.com>
2024-07-25 15:28:43 +0300

    Added getRecord Function

--- src/MedicalRecord.cpp
+++ src/MedicalRecord.cpp
@@ -1,25 +1,33 @@
 #include "../include/MedicalRecord.h"
 
-
-MedicalRecord::MedicalRecord(int patientId,int doctorId)
+MedicalRecord::MedicalRecord(int patientId, int doctorId)
 {
-   this->patientId = patientId;
-   this->doctorId  = doctorId;
-   this->recordId  = nextID++;
+    this->patientId = patientId;
+    this->doctorId = doctorId;
+    this->recordId = nextID++;
 }
 
-void MedicalRecord::setDiagnosisResult(std::string diagnosisResult){
+void MedicalRecord::setDiagnosisResult(std::string diagnosisResult)
+{
     this->diagnosisResult = diagnosisResult;
 }
 
-void MedicalRecord::setMedicalNeeded( std::string medicalNeeded){
+void MedicalRecord::setMedicalNeeded(std::string medicalNeeded)
+{
     this->medicalNeeded = medicalNeeded;
 }
 
-std::string MedicalRecord::viewDiagnosis(){
+std::string MedicalRecord::viewDiagnosis()
+{
     return diagnosisResult;
 }
 
-std::string MedicalRecord::viewMedicalNeeded(){
+std::string MedicalRecord::viewMedicalNeeded()
+{
     return medicalNeeded;
+}
+
+std::string MedicalRecord::getMedicalRecord()
+{
+    return "Record ID: " + std::to_string(recordId) + "\nPatient ID: " + std::to_string(patientId) + "\nDoctor ID: " + std::to_string(doctorId) + "\nDiagnosis: " + diagnosisResult + "\nMedical Needed: " + medicalNeeded;
 }
