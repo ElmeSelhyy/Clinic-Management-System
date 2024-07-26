925e115b2cf6392899f55da9c7e1f21356ddfa0d
ahmedyasser20 <ahmedyasser200211@gmail.com>
2024-07-26 20:59:39 +0300

    fix get availableSlot bug

--- src/Schedule.cpp
+++ src/Schedule.cpp
@@ -5,7 +5,7 @@
 #include <algorithm>
 #include <string>
 #include <sstream>
-#include <fstream> 
+#include <fstream>
 Schedule::Schedule()
 {
     // Default constructor
@@ -19,10 +19,8 @@ Schedule::Schedule(std::string user_id, int user_type)
 void Schedule::addSlot(std::string &slot)
 {
     slots.push_back(slot);
-     FileHandler fileHandler = FileHandler(SLOT_FILE);
+    FileHandler fileHandler = FileHandler(SLOT_FILE);
     fileHandler.writeFile(slot);
-
-
 }
 
 bool Schedule::removeSlot(std::string &slot)
@@ -30,14 +28,13 @@ bool Schedule::removeSlot(std::string &slot)
     for (int i = 0; i < slots.size(); i++)
     {
         if (slots[i] == slot)
-        
+
         {
             slots.erase(slots.begin() + i);
             break;
         }
     }
 
-    
     std::vector<std::string> words;
     std::istringstream iss(slot);
     // Extract words from the line
@@ -48,14 +45,16 @@ bool Schedule::removeSlot(std::string &slot)
     std::ifstream infile(SLOT_FILE);
     std::ofstream outfile(TEMP_FILE);
     std::string line;
-    bool changed=false;
+    bool changed = false;
 
-    while (std::getline(infile, line)) {
-        if (line.find(words[0]) != std::string::npos && 
-            line.find(words[1]) != std::string::npos && 
-            line.find(words[2]) != std::string::npos){
+    while (std::getline(infile, line))
+    {
+        if (line.find(words[0]) != std::string::npos &&
+            line.find(words[1]) != std::string::npos &&
+            line.find(words[2]) != std::string::npos)
+        {
 
-            changed=true;
+            changed = true;
             continue;
         }
         outfile << line << std::endl;
@@ -63,18 +62,18 @@ bool Schedule::removeSlot(std::string &slot)
     infile.close();
     outfile.close();
 
-    if(changed){
+    if (changed)
+    {
         remove(SLOT_FILE);
         rename(TEMP_FILE, SLOT_FILE);
     }
-    else{
-       
+    else
+    {
+
         remove(TEMP_FILE);
         return false;
     }
     return true;
-
-    
 }
 
 std::vector<std::string> Schedule::getavailableSlots()
@@ -85,7 +84,7 @@ std::vector<std::string> Schedule::getavailableSlots()
     {
         std::istringstream iss(slots[i]);
         std::string patientName;
-        iss >> patientName;
+        iss >> patientName >> patientName;
         if (patientName == "NULL")
         {
             slots.push_back(slots[i]);
